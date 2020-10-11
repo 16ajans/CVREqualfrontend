88ae84340301e02c52bc2dc212f009f5ed21a4e8
16ajans <alexjansen210@gmail.com>
2020-10-10 21:42:01 -0500

    i hate this

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,18 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
-Vue.use(Vuetify);
+import colors from 'vuetify/lib/util/colors'
+
+Vue.use(Vuetify)
 
 export default new Vuetify({
-});
+  theme: {
+    themes: {
+      light: {
+        primary: colors.deepPurple,
+        secondary: colors.black,
+        // accent: colors.lolwut,
+      },
+    },
+  },
+})
--- vue.config.js
+++ vue.config.js
@@ -3,3 +3,13 @@ module.exports = {
     "vuetify"
   ]
 }
+/*module.exports = {
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'Your new title'
+        return args
+      })
+  }
+}*/
