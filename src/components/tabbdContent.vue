f1533b2093ef357dd256041452bb110ca99995ec
16ajans <alexjansen210@gmail.com>
2020-10-11 14:36:00 -0500

    final? mockup

--- src/components/tabbdContent.vue
+++ src/components/tabbdContent.vue
@@ -7,26 +7,50 @@
       ><v-tabs-items v-model="tab"
         ><v-tab-item
           ><v-card elevation="2" class="ma-2" v-for="n in 10" :key="n"
-            ><v-card-title>SongName %%.%% - Player Name (School)</v-card-title>
-            <v-card-text>000,000 points - date time</v-card-text></v-card
+            ><v-card-title
+              ><span class="avoidwrap">SongName %%.%%</span><pre> - </pre>
+              <span class="avoidwrap"> Player Name (School)</span></v-card-title
+            >
+            <v-card-subtitle
+              >000,000 points - date time</v-card-subtitle
+            ></v-card
           ></v-tab-item
         ><v-tab-item
-          ><v-card elevation="2" class="ma-2"
-            ><v-card-title>D1</v-card-title
-            ><v-list-item class="ma-2" v-for="n in 16" :key="n"
-              ><v-card-text>{{ n }}</v-card-text></v-list-item
-            ></v-card
-          ><v-card elevation="3" class="ma-2"
-            ><v-card-title>D2</v-card-title
-            ><v-list-item class="ma-2" v-for="n in 16" :key="n"
-              ><v-card-text>{{ n }}</v-card-text></v-list-item
-            ></v-card
+          ><v-card elevation="2" class="ma-2">
+            <v-simple-table>
+              <template v-slot:default>
+                <thead>
+                  <tr>
+                    <th class="text-left">Rank</th>
+                    <th class="text-left">Player</th>
+                    <th class="text-left">School</th>
+                    <th class="text-left">Plays</th>
+                    <th class="text-left">Score</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="n in 16" :key="n">
+                    <td>{{ n }}</td>
+                    <td>Username</td>
+                    <td>School</td>
+                    <td>0</td>
+                    <td>000,000</td>
+                  </tr>
+                </tbody>
+              </template>
+            </v-simple-table></v-card
           ></v-tab-item
         ></v-tabs-items
       >
     </v-card>
   </v-app>
 </template>
+
+<style>
+span.avoidwrap {
+  display: inline-block;
+}
+</style>
 
 <script>
 export default {
